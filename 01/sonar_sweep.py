25b17fd7c0633d35b9c6f3d05bceb4d3eb6d79f3
jk464 <scott.swann@hotmail.co.uk>
2021-12-01 11:58:28 +0000

    AL6 feedback

--- 01/sonar_sweep.py
+++ 01/sonar_sweep.py
@@ -1,25 +1,6 @@
 import argparse
 
 
-def window_total(measurements, window):
-    """Gets the total sum of measurements across an arbitrary range length (window)
-
-    Parameters:
-      measurements (list): List of measurements to take values from
-      window       (int) : Size of range to sum over
-
-    Returns:
-      total (int): Total of the sum over the window
-    """
-
-    total = 0
-
-    for i in range(window):
-        total += measurements[i]
-
-    return total
-
-
 def find_increase_count(measurements, window=1):
     """For a given set of measurements and for a give window size, find how many times there's an increase in the sum of consecutive measurements
 
@@ -35,9 +16,9 @@ def find_increase_count(measurements, window=1):
     higher_count = 0
     i = 0
     for measurement in measurements[: len(measurements) - window + 1]:
-        total = window_total(measurements[i : i + window], window)
+        total = sum(measurements[i : i + window])
 
-        if previous_measurement is not None and total > previous_measurement:
+        if previous_measurement and total > previous_measurement:
             higher_count += 1
 
         previous_measurement = total
@@ -63,10 +44,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Read in inputs file - also ensure we store values as ints
-    with open(args.inputs) as file:
-        inputs = []
-        for line in file.readlines():
-            inputs.append(int(line))
+    with open(args.inputs, "r") as file:
+        inputs = [int(l) for l in file.readlines()]
 
     # Find our answer :)
     print(find_increase_count(inputs, window=args.window))
