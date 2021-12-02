86fe5704e6600c438429168af9233d9b676650b9
jk464 <scott.swann@hotmail.co.uk>
2021-12-02 14:47:39 +0000

    Restructure - missing day 2.1

--- src/02/dive.py
+++ src/02/dive.py
@@ -2,11 +2,12 @@ import argparse
 from typing import List  # Python 3.8 and earlier
 
 
-def calculate_moved_distance(movements: list) -> int:
+def calculate_moved_distance(movements: list, use_aim: bool = False) -> int:
     """For a given set of movement inputs calculates the horizontal and vertical distance moved and returns the product of the two values
 
     Parameteres:
       movements (list): List of movement inputs - Each list entry, is a pair of [movement direction, movement value]
+      use_aim (bool): Define whether up / down movements indicated a change in depth or aim.
 
     Returns
       movement_product (int): Product of the horizontal and vertical distance moved
@@ -19,11 +20,18 @@ def calculate_moved_distance(movements: list) -> int:
     for move in movements:
         if move[0] == "forward":
             x += move[1]
-            y += move[1] * aim
+            if use_aim:
+                y += move[1] * aim
         elif move[0] == "down":
-            aim += move[1]
+            if use_aim:
+                aim += move[1]
+            else:
+                y += move[1]
         elif move[0] == "up":
-            aim -= move[1]
+            if use_aim:
+                aim -= move[1]
+            else:
+                y -= move[1]
 
     movement_product = x * y
 
@@ -36,6 +44,7 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--inputs", help="Inputs file with values")
+    parser.add_argument('--aim', action='store_true', default=False)
 
     args = parser.parse_args()
 
@@ -44,4 +53,4 @@ if __name__ == "__main__":
         inputs = [[l.split(" ")[0], int(l.split(" ")[1])] for l in file.readlines()]
 
     # Find our answer :)
-    print(calculate_moved_distance(inputs))
+    print(calculate_moved_distance(inputs, args.aim))
