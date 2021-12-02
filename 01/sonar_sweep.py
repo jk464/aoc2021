44b90c8987961840d8433e013790f5162b9d904c
jk464 <44260911+jk464@users.noreply.github.com>
2021-12-02 10:10:10 +0000

    AOC 2021 Day 01 (#1)
    * AOC 2021 Day 01
    
    * Test ci (#2)
    
    * CI TEST
    
    * ci in right place
    
    * Does this thing have python?
    
    * Does this thing have python? - correct place
    
    * Test my code
    
    * Test my code - purposeful break
    
    * Test my code - fix
    
    * Day 01 Ci - part 1 and 2
    
    * name change
    
    * name change
    
    * Black formatting
    
    * AL6 feedback
    
    * Adding type hinting

---
+++ 01/sonar_sweep.py
@@ -0,0 +1,51 @@
+import argparse
+from typing import List  # Python 3.8 and earlier
+
+def find_increase_count(measurements: List[int], window: int=1) -> int:
+    """For a given set of measurements and for a give window size, find how many times there's an increase in the sum of consecutive measurements
+
+    Parameters:
+      measurements (list): List of measurements to take values from
+      window       (int) : Size of range to sum over
+
+    Returns:
+      higher_count (int): How many times there's an increase in the sum of consecutive measurements
+    """
+
+    previous_measurement = None
+    higher_count = 0
+    i = 0
+    for measurement in measurements[: len(measurements) - window + 1]:
+        total = sum(measurements[i : i + window])
+
+        if previous_measurement and total > previous_measurement:
+            higher_count += 1
+
+        previous_measurement = total
+        i += 1
+
+    return higher_count
+
+
+# Only run if called from the command line
+if __name__ == "__main__":
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputs", help="Inputs file with measurement values")
+    parser.add_argument(
+        "--window",
+        help="Size of rolling measurement window, default 1",
+        nargs="?",
+        const="default",
+        default=1,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    # Read in inputs file - also ensure we store values as ints
+    with open(args.inputs, "r") as file:
+        inputs = [int(l) for l in file.readlines()]
+
+    # Find our answer :)
+    print(find_increase_count(inputs, window=args.window))
